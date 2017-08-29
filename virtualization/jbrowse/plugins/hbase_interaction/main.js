77699b31e852689e3b27cd0ec8f25906f13ab433
sc724 <kz724@outlook.com>
2017-08-29 20:52:37 +0000

    start to work on plugin

---
+++ virtualization/jbrowse/plugins/hbase_interaction/main.js
@@ -0,0 +1,19 @@
+define([
+           'dojo/_base/declare',
+           'JBrowse/Plugin'
+       ],
+       function(
+           declare,
+           JBrowsePlugin
+       ) {
+ 
+return declare( JBrowsePlugin,
+{
+    constructor: function( args ) {
+        var browser = this.browser;
+ 
+        /* do anything you need to initialize your plugin here */
+    }
+});
+ 
+});
