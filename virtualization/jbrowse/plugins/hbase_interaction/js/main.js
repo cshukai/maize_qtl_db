ca5458cb5cadfa87a637bbc07f786d5265de0757
sc724 <kz724@outlook.com>
2017-08-29 21:09:52 +0000

    save before go home

---
+++ virtualization/jbrowse/plugins/hbase_interaction/js/main.js
@@ -0,0 +1,24 @@
+define([
+           'dojo/_base/declare',
+           'dojo/_base/array',
+           'dojo/request/xhr',
+           'JBrowse/ConfigManager',
+           'JBrowse/Plugin'
+       ],
+       function(
+           declare,
+           array,
+           xhr,
+           ConfigManager,
+           JBrowsePlugin
+       ) {
+ 
+            return declare( JBrowsePlugin,{
+                        constructor: function( args ) {
+                            var browser = this.browser;
+ 
+        /* do anything you need to initialize your plugin here */
+                        }
+});
+ 
+});
