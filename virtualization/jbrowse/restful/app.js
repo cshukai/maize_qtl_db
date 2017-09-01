196b5130d582c43c1689b077a6cef5d40b9500b6
sc724 <kz724@outlook.com>
2017-09-01 15:21:44 +0000

    got node express send html file

--- virtualization/jbrowse/restful/app.js
+++ virtualization/jbrowse/restful/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 // Creates an Express application using the top-level function
 const app = express();
-const port = 5000;
+const port = 5001;
 
 app.get('/distribution', function(request, response) {
-  response.send('Hello, World!');
+        response.sendFile('/root/docker-jbrowse/restful/output.html');
+});
+
+// Make the app listen on port 3000
+app.listen(port, function() {
+  console.log('Server listening on http://localhost:' + port);
 });
