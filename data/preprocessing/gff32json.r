c3467533144364c32eee6019ded6529982ab4fa9
sc724 <kz724@outlook.com>
2017-09-22 04:31:06 +0000

    json format fix

--- data/preprocessing/gff32json.r
+++ data/preprocessing/gff32json.r
@@ -1,24 +1,26 @@
  library(jsonlite)
  
- qtl_d=read.table("example.gff",sep="\t",header=T)
+ qtl_d=as.matrix(read.table("example.gff",sep="\t",header=T))
  chrs=as.numeric(names(table(qtl_d[,"chr"])))
- #trait=qtl_d[,"Trait"]
- #start_pos=qtl_d[,"start"]
- #end_pos=qtl_d[,"end"]
- #evidence_type=qtl_d[,"evidence_type"]
- #authors=qtl_d[,"reference"]
+
  annots=NULL
  for(i in 1:length(chrs)){
-     d=qtl_d[which(qtl_d[,"chr"]==chrs[i]),]
-     these_len=d[,"end"]-d[,"start"]
+     d=qtl_d[which(as.numeric(qtl_d[,"chr"])==chrs[i]),]
+     d=as.matrix(d)
+     if(ncol(d)==1){
+         d=t(d)
+     }
+     colnames(d)=colnames(qtl_d)
+     these_len=as.numeric(d[,"end"])-as.numeric(d[,"start"])
      qtl_embedded=list()
-     #these_type=d[,"evidence_type"]
-     #these_auth=d[,"reference"]
-     #these_trait=d[,"Trait"]
+    
      these_trait=NULL
      for(j in  1:nrow(d)){
          if(d[j,"Trait"]=="Ear_Number"){
-             
+             d[j,"Trait"]=1
+         }
+         if(as.character(d[j,"evidence_type"])=="f2"){
+             d[j,"evidence_type"]=1
          }
      }
      this_set=cbind(d[,"reference"],d[,"start"],these_len,d[,"Trait"],d[,"evidence_type"])
@@ -27,9 +29,11 @@
      annots=rbind(annots,this.row)
      
  }
- 
- 
+ colnames(annots)=c("chr","annots")
+ annots_2=as.data.frame(annots)
+
+ result=list()
  result[[1]]=c("name","start","length","expression-level","gene-type" )
- result[[2]]=annots
+ result[[2]]=annots_2
  names(result)=c("keys","annots")
- toJSON(result)
+ out=toJSON(result)
