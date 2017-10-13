c4dcb7800a422a967e01d2d730242c9027afb6e8
sc724 <kz724@outlook.com>
2017-10-13 04:03:41 +0000

    map  phenotype name with qtl exp

--- data/statistics/maizegdb/qtl.r
+++ data/statistics/maizegdb/qtl.r
@@ -1,11 +1,15 @@
+#refence : https://ftp.maizegdb.org/MaizeGDB/FTP/MaizeGDB_MySQL/MaizeGDBSchema.pdf
 library(RMySQL)
-#summary: qtl exp
 con = dbConnect(RMySQL::MySQL(), dbname = "sc724")
-d=dbReadTable(con, "QTL_LINK_EXP")
+d=dbReadTable(con, "QTL_EXP")
 d2=dbReadTable(con, "TRAIT_ANALYSIS")
 d3=dbReadTable(con, "PHENOTYPE")
 
+#locate useful qtl information
+##phenotype
 result=d2[which(d2[,"TRAIT"] %in% intersect(d3[,"TRAIT"],d2[,"TRAIT"])),c("TRAIT","NAME","QTL_EXP","EXPERIMENTAL_DESIGN","HERITABILITY")]
+##qtl and evidence type (ex:f2)
+qtl_exp=d[which(d[,"ID"] %in% result[,"QTL_EXP"]),] 
 
 dbDisconnect(con)
 
