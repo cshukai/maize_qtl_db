dd9ccceea926668087c494ad76d06478e428ede4
sc724 <kz724@outlook.com>
2017-10-25 20:20:55 +0000

    translated link group to chromsome

--- data/statistics/maizegdb/qtl.r
+++ data/statistics/maizegdb/qtl.r
@@ -7,10 +7,24 @@ d3=dbReadTable(con, "PHENOTYPE")
 d4=dbReadTable(con, "QTL_LINK_ANALYSIS")
 d5=dbReadTable(con, "PHENOTYPE_TRAIT")
 d6=dbReadTable(con, "LOCUS")
-#d6 contains qtl train and bin information
-qtl=d6[which(d6[,"TYPE"]==25396),]
+d7=dbReadTable(con, "LINKAGE_GROUP")
+## qtl processing :d6 contains qtl train and bin information
+qtl=d6[intersect(which(d6[,"TYPE"]==25396),which(!is.na(d6[,"LINKAGE_GROUP"]))),]  # 25396 referes to locus that are assocaited with quantitative trait
+# convert linkdage group to chromsome
+chrs=NULL
+for(i in 1:nrow(qtl)){
+   this.link.group=qtl[i,"LINKAGE_GROUP"]#linkage group referes to chromosomes  
+   this.chr=d7[which(d7[,"ID"]==this.link.group),"CHR_"]
+   if(length(this.chr)==0){
+       print(qtl[i,"LINKAGE_GROUP"])
+   }
+   chrs=c(chrs,this.chr)
+} 
+qtl2=cbind(qtl,chrs)
+colnames(qtl2)=c(colnames(qtl),"chrs")
+#value refers to bin location 
 
-#locate useful qtl information
+
 ##phenotype
 result=d2[which(d2[,"TRAIT"] %in% intersect(d3[,"TRAIT"],d2[,"TRAIT"])),c("TRAIT","NAME","QTL_EXP","EXPERIMENTAL_DESIGN","HERITABILITY")]
 ##qtl and evidence type (ex:f2)
@@ -18,3 +32,5 @@ qtl_exp=d[which(d[,"ID"] %in% result[,"QTL_EXP"]),]
 
 ## todo- identifided which qtl are included in  47 experiments and where are they located
 dbDisconnect(con)
+
+save.image()
